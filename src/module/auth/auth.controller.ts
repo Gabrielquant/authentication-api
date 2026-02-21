0b3b0dbf7c570d9c199c8e44b4c0b04ccc2a9a6f
Gabriel Ferreira <gabriel.ferreiraa023@gmail.com>
2026-02-21 18:50:54 -0300

    Feat: Implemt swagger documentation

--- src/module/auth/auth.controller.ts
+++ src/module/auth/auth.controller.ts
@@ -6,36 +6,43 @@ import { AuthService } from "./auth.service";
 import { LoginDto } from "./dto/login.dto";
 import { ResetUserPasswordDto } from "./dto/reset-password.dto";
 import { ResetPasswordRequest } from "./dto/reset-password-request.dto";
+import { ApiBearerAuth, ApiOperation } from "@nestjs/swagger";
 
 @Controller("auth")
 export class AuthController {
 	constructor(private readonly authService: AuthService) {}
 
 	@Post("register")
+	@ApiOperation({ summary: "Cria a conta do usuario" })
 	registerUser(@Body() createUserDto: CreateUserDto) {
 		return this.authService.register(createUserDto);
 	}
 
 	@Throttle({default: { limit: 3, ttl: 60000 }})
 	@Post("login")
+	@ApiOperation({ summary: "Loga o usuario e retorna o token JWT" })
 	logIn(@Body() logInDto: LoginDto) {
 		return this.authService.generateJwt(logInDto);
 	}
 
 	@Post("refresh")
 	@UseGuards(JwtRefreshGuard)
+	@ApiBearerAuth()
+	@ApiOperation({ summary: "Realiza o update de email do usuario" })
 	refresh(@Req() req) {
 		return this.authService.refreshTokens(req.user.email);
 	}
 
 	@Throttle({default: { limit: 3, ttl: 60000 }})
 	@Post("request/reset/password")
+	@ApiOperation({ summary: "Realiza o update de email do usuario" })
 	requestResetPassword(@Body() resetPasswordRequest: ResetPasswordRequest) {
 		return this.authService.requestResetPassword(resetPasswordRequest);
 	}
 
 	@Throttle({default: { limit: 3, ttl: 60000 }})
 	@Patch("reset/password")
+	@ApiOperation({ summary: "Realiza o update de email do usuario" })
 	resetPassword(@Body() resetUser: ResetUserPasswordDto) {
 		return this.authService.resetPassword(resetUser);
 	}
--- src/module/users/users.controller.ts
+++ src/module/users/users.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
+import { ApiBearerAuth, ApiOperation } from "@nestjs/swagger";
 import { CurrentUser } from "src/common/decorator/current-user.decorator";
 import { Roles } from "src/common/decorator/roles.decorator";
 import { JwtAuthGuard } from "src/common/guard/auth-access-token.guard";
@@ -12,6 +13,8 @@ export class UsersController {
 	constructor(private readonly userService: UsersService) {}
 
 	@UseGuards(JwtAuthGuard)
+	@ApiBearerAuth()
+	@ApiOperation({ summary: "Realiza o update de email do usuario" })
 	@Post("update")
 	updateUser(
 		@Body() updateUserDto: UpdateUserDto,
@@ -21,7 +24,9 @@ export class UsersController {
 	}
 
 	@UseGuards(JwtAuthGuard, RolesGuard)
+	@ApiBearerAuth()
 	@Roles(Role.ADMIN)
+	@ApiOperation({ summary: "Buscar todas as iformações do usuario" })
 	@Get()
 	getUser(@CurrentUser() payloadDto: PayloadDto) {
 		return this.userService.getUser(payloadDto);
